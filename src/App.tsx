cc254d877d4c7a2ea65cc1f4ec9f9300a4ef69d8
Afolabi Esther <starafolab@gmail.com>
2024-03-27 17:58:02 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <>
+        
+        <div className='bg-slate-500 text-slate-100'> hello welcome to the store</div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./public/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
